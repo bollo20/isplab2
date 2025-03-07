620d8b9bd0277bc26179376331fadc784ac815c1
bollo20 <valentinateodora.bolovanica@ulbsibiu.ro>
2025-03-07 11:00:31 +0200

    laborator2

---
+++ src/main/java/ro/ulbs/paradigme/lab2/Triangle.java
@@ -0,0 +1,19 @@
+package ro.ulbs.paradigme.lab2;
+
+public class Triangle {
+    private float height;
+    private float base;
+    public Triangle(){
+        super(color);
+        this.height=0;
+        this.base=0;
+    }
+    public Triangle(String color,float,super(color));
+    this.height=height;
+    this.base=base;
+
+}
+  @Override
+ public float getArea(){
+    return(base*height) /2;
+  }
