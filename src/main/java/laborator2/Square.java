f1c9d51492b80636306bfcf2dc58552479ef7f66
bollo20 <valentinateodora.bolovanica@ulbsibiu.ro>
2025-05-27 17:40:55 +0300

    laborator2

---
+++ src/main/java/laborator2/Square.java
@@ -0,0 +1,26 @@
+package laborator2;
+
+public class Square extends Form {
+    private float side;
+
+    public Square() {
+        super();
+        this.side = 0;
+    }
+
+    public Square(float side, String color) {
+        super(color);
+        this.side = side;
+    }
+
+    @Override
+    public float getArea() {
+        return side * side;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", square with side " + side;
+    }
+}
+
